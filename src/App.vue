fafc99db5160d919875f2e4123aecb3471de28f4
truejojo <johannesveh@yahoo.de>
2023-07-04 10:20:36 +0200

    clean up index.html App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
 import { RouterView } from 'vue-router'
-import TheHeader from './components/TheHeader.vue';
+import TheHeader from './components/TheHeader.vue'
 import TheFooter from './components/TheFooter.vue'
 </script>
 
 <template>
-  <div>
+  <div class="vue-app bg-dark text-white">
     <TheHeader />
     <main class="container">
       <RouterView />
@@ -13,3 +13,12 @@ import TheFooter from './components/TheFooter.vue'
     <TheFooter />
   </div>
 </template>
+
+<style lang="scss" scoped>
+.vue-app {
+  min-height: 100vh;
+  width: 100vw;
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+}
+</style>
