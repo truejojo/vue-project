0c7ddf65563a5992fbf17aa71b8ed1f6ae6dd202
truejojo <johannesveh@yahoo.de>
2023-08-01 10:43:45 +0200

    save the current state

---
+++ dotenv.js
@@ -0,0 +1,12 @@
+// import dotenv from 'dotenv'
+
+// dotenv.config()
+
+// export const firebaseConfig = {
+//   apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.VUE_APP_FIREBASE_APP_ID
+// }
